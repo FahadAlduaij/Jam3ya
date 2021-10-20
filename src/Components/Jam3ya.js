1869db16d530d999fc3ded1d713fadd11aefae3f
fahadalduaij <fahad_osama@hotmail.com>
2021-10-20 16:05:49 +0300

    fixing jam3ya details Title and <p>

--- src/Components/Jam3ya.js
+++ src/Components/Jam3ya.js
@@ -10,8 +10,6 @@ function Jam3ya(props) {
 	const startDateJam3ya = new Date(props.jam3ya.startDate);
 	const endDateJam3ya = new Date(props.jam3ya.endDate);
 
-	
-
 	return (
 		<div className="card-container">
 			<Card style={{ width: "18rem" }}>
